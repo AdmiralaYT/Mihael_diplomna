738f86c584ea77fddf01faab82c3aceb6fac013b
AdmiralaMTB <48565515+AdmiralaYT@users.noreply.github.com>
2023-05-20 23:28:18 +0300

    Add files via upload

---
+++ bttest.ino
@@ -0,0 +1,22 @@
+#include <SoftwareSerial.h>
+
+const byte rxPin = 3;
+const byte txPin = 2;
+
+SoftwareSerial mySerial (rxPin, txPin);
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  Serial.println("AT");
+  delay(500);
+  if (Serial.available() > 0) {
+    String response = Serial.readString();
+    Serial.println("Отговор от HC-05: " + response);
+  }
+
+  delay(1000);
+}
